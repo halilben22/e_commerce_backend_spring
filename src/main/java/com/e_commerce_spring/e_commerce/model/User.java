6d00fdf1aa6d0adf37d83a0abe5640d240cb8136
halilben22 <halilattar01@gmail.com>
2025-12-24 21:49:24 +0100

    user-model added

--- src/main/java/com/e_commerce_spring/e_commerce/model/User.java
+++ src/main/java/com/e_commerce_spring/e_commerce/model/User.java
@@ -1,4 +1,42 @@
 package com.e_commerce_spring.e_commerce.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true) // Email benzersiz olmalı
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private Boolean isActive;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
