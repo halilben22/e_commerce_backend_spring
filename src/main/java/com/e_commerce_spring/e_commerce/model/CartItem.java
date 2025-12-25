666d9f8e8f6898d751d8e65ae502b34e659e0764
halilben22 <halilattar01@gmail.com>
2025-12-25 15:47:28 +0100

    cart-item model added

---
+++ src/main/java/com/e_commerce_spring/e_commerce/model/CartItem.java
@@ -0,0 +1,30 @@
+package com.e_commerce_spring.e_commerce.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+}
