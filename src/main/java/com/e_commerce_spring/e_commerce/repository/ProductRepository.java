aae819f72033646691e773e55d53b643ec282ca1
halilben22 <halilattar01@gmail.com>
2025-12-25 16:25:27 +0100

    product repository added

---
+++ src/main/java/com/e_commerce_spring/e_commerce/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.e_commerce_spring.e_commerce.repository;
+
+import com.e_commerce_spring.e_commerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
